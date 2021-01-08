aed43561904db2f1a3116a5e0dd3da1b2ab70abe
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-01-08 21:14:23 +0300

    Add errors processing.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const getResource = async (url) => {
   const result = await fetch(url);
+
+  if (!result.ok) {
+    throw new Error(`Could not fetch ${url}, received ${result.status}`);
+  }
+
   const body = await result.json();
   return body;
 };
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/123123/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.error('Could not fetch', err);
   });
