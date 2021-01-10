235027dae0b9958ee51968c4c66d4c7f72cdd906
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-01-10 23:01:11 +0300

    Create folder for services (move SwapiService class).

---
+++ services/swapi-service.js
@@ -0,0 +1,49 @@
+export default class SwapiService {
+  _apiBase = `https://swapi.dev/api`;
+
+  async getResource(url) {
+    const result = await fetch(`${this._apiBase}${url}`);
+
+    if (!result.ok) {
+      throw new Error(`Could not fetch ${url}, received ${result.status}`);
+    }
+
+    const body = await result.json();
+    return body;
+  }
+
+  async getAllPeople() {
+    const result = await this.getResource(`/people/`);
+    return result.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const result = await this.getResource(`/planets/`);
+    return result.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
+
+  async getAllStarships() {
+    const result = await this.getResource(`/starships/`);
+    return result.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+  people.forEach(person => {
+    console.log(person.name);
+  });
+});
